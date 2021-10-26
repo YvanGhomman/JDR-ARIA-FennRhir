71e49bb0cc3cdf4653fc13ea8ecaee45f8578c5a
YvanGhomman <yvan.ghomman.yg@gmail.com>
2021-10-26 18:45:45 +0200

    test2 heroku

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -4,5 +4,5 @@ module.exports = {
     HOST: /* process.env.DB_HOST */"eu-cdbr-west-01.cleardb.com",
     USER: /* process.env.DB_USER */"b6017d09c3b5cf",
     PASSWORD: /* process.env.DB_PASSW */"d9d84aed",
-    DB: /* process.env.DB_DB */"jdr-aria"
+    DB: /* process.env.DB_DB */"heroku_94b94d867de9aff"
   };
