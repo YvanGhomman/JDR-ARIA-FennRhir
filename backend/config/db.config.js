92d4eae56617d3645e955d22223781b9c657a37d
YvanGhomman <yvan.ghomman.yg@gmail.com>
2021-10-26 18:23:46 +0200

    test mysql heroku

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASSW,
-    DB: process.env.DB_DB
+    HOST: /* process.env.DB_HOST */"eu-cdbr-west-01.cleardb.com",
+    USER: /* process.env.DB_USER */"b6017d09c3b5cf",
+    PASSWORD: /* process.env.DB_PASSW */"d9d84aed",
+    DB: /* process.env.DB_DB */"jdr-aria"
   };
